d67df0d0165f6e3e72185a984d045f7682fe763c
Eple <konieczny.tomek@gmail.com>
2013-08-09 21:48:16 +0200

    Change structure.

--- 1_1_1.R
+++ 1_1_1.R
@@ -1,14 +1,27 @@
-f_x <- function(x, a, b){
-  a * x + b
+#Functions and graphs from chapter 1
+#Fig.I.1.1 - linear funcion
+f_x <- function(x){
+  3 * x + 2
 }
-
-#define the domain and parameters of the linear function
-x <- seq(-2.5, 2.5, len = 300); a <- 3; b <- 2
-y <- f_x(x, a, b)
-plot(x, y, 
+curve(f_x, -2.5, 2.5, 
      col = "red", 
      type = "l", 
      lwd = 2, 
      main = "Linear function", 
      xlab = "x-values", ylab = "y-values", cex.lab = 0.75)
+abline(h = 0, v = 0, lty = 5, col = "black")
+grid()
+
+#Fig.I.1.2 - quadratic funcion
+f_x <- function(x){
+  4 * x ^ 2 + 3 * x + 2
+}
+curve(f_x, -2.5, 2.5,
+     col = "red", 
+     type = "l", 
+     lwd = 2, 
+     main = "Quadratic function", 
+     xlab = "x-values", ylab = "y-values", cex.lab = 0.75,
+     ylim = c(0, 35))
+abline(h = 0, v = 0, lty = 5, col = "black")
 grid()
