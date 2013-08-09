d663d0995ee3a703f4e7da6c7ff885bf038dd6fd
Eple <konieczny.tomek@gmail.com>
2013-08-09 02:12:17 +0200

    Add first functions.

---
+++ 1_1_1.R
@@ -0,0 +1,14 @@
+f_lin <- function(x, a, b){
+  a * x + b
+}
+
+#define the domain and parameters of the linear function
+x <- seq(-2.5, 2.5, len = 300); a <- 3; b <- 2
+y <- f_lin(x, a, b)
+plot(x, y, 
+     col = "red", 
+     type = "l", 
+     lwd = 2, 
+     main = "Linear function", 
+     xlab = "x-values", ylab = "y-values", cex.lab = 0.75)
+grid()
---
+++ 1_1_2.R
@@ -0,0 +1,14 @@
+f_quad <- function(x, a, b, c){
+  a * x ^ 2 + b * x + c
+}
+
+#define the domain and parameters of the quadratic function
+x <- seq(-2.5, 2.5, len = 300); a <- 4; b <- 3; c <- 2 
+y <- f_quad(x, a, b, c)
+plot(x, y, 
+     col = "red", 
+     type = "l", 
+     lwd = 2, 
+     main = "Quadratic function", 
+     xlab = "x-values", ylab = "y-values", cex.lab = 0.75)
+grid()
