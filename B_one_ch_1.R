1b8c9cab5e87fc0323574b8518aca6f7927792fc
Eple <konieczny.tomek@gmail.com>
2013-08-12 08:40:10 +0200

    Add new sections.

--- B_one_ch_1.R
+++ B_one_ch_1.R
@@ -104,3 +104,45 @@ points(root_1, 0, col = "dark green")
 points(root_2, 0, col = "dark green")
 abline(v = root_1, lwd = 1, lty = 4, col = "grey")
 abline(v = root_2, lwd = 1, lty = 4, col = "grey")
+
+#Sec.I.1.3.4 - Stationary points
+#define the initial funtion
+initF <- function(x){
+  x^2*log(x)
+}
+#calculate (define) the derivative of the initial function
+fstDeriv <- function(x){
+  2*x*log(x) + x
+}
+sndDeriv <- function(x){
+  2*log(x) + 3
+}
+#plotting procedures
+curve(initF, 0, 1.5,
+      main = "Finding saddle points", xlab = "x", ylab = "y",
+      ylim = c(-1, 1),
+      col = "red", lwd = 2,
+      cex.lab = 0.75)
+curve(fstDeriv, add = TRUE, col = "dark green", lty = 2)
+curve(sndDeriv, add = TRUE, col = "green", lty = 2)
+abline(h = 0, col = "grey", lty = 4)
+#calculate the roots of the derivate (stationary points)
+root_1 <- uniroot(deriv, c(0.2, 1))$root
+root_2 <- uniroot(deriv, c(3, 4))$root
+points(root_1, 0, col = "dark green")
+points(root_2, 0, col = "dark green")
+abline(v = root_1, lwd = 1, lty = 4, col = "grey")
+abline(v = root_2, lwd = 1, lty = 4, col = "grey")
+
+#Sec.I.1.3.5 - Integration (Fig. I.1.9)
+f_1 <- function(x) {
+  2*x + x^2 
+}
+curve(f_1, 0, 5,
+      main = "The definite integral",
+      xlab = "x", ylab = "f(x)",
+      cex.lab = 0.75)
+grid()
+x.rng <- c(2, seq(2,4, len = 100), 4)
+y.rng <- c(0, f_1(seq(2,4,len = 100)), 0)
+polygon(x.rng, y.rng, col = "grey", border = "grey")
